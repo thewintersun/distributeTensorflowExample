c8e85341a1a80fce76a52996c16198556bdad186
luodongri <luodongri@gmail.com>
2016-09-18 17:18:37 +0800

    fix bug

--- distribute.py
+++ distribute.py
@@ -59,7 +59,7 @@ def main(_):
                             save_model_secs=60)      
     with sv.managed_session(server.target) as sess:
       step = 0
-      while  not sv.should_stop() and  step < 1000000:
+      while  step < 1000000:
         train_x = np.random.randn(1)
         train_y = 2 * train_x + np.random.randn(1) * 0.33  + 10
         _, loss_v, step = sess.run([train_op, loss_value,global_step], feed_dict={input:train_x, label:train_y})
@@ -67,6 +67,7 @@ def main(_):
           w,b = sess.run([weight,biase])
           print("step: %d, weight: %f, biase: %f, loss: %f" %(step, w, b, loss_v))
 
+    sv.stop()
 
 def loss(label, pred):
   return tf.square(label - pred)
